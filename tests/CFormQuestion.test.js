222e05b40ed4a2e64b6372dcf9a36896ac69f4b9
Robin Scholtes <robin@theweirdscience.com>
2023-09-19 17:59:22 +1200

    basic test setup

--- tests/CFormQuestion.test.js
+++ tests/CFormQuestion.test.js
@@ -1,8 +1,11 @@
 import {CFormQuestion} from "../src/scripts/CFormQuestion.js";
 
 class CMock extends CFormQuestion {
-    updateIsValid(){}
-    handleKeyup(){}
+    updateIsValid = jest.fn()
+    handleKeyup = jest.fn()
+    buildHelperElement = jest.fn().mockImplementation(() => super.buildHelperElement())
+    setHelperText = jest.fn().mockImplementation(() => super.setHelperText('error', 'This field is required'))
+    setAsterisk = jest.fn().mockImplementation(() => super.setAsterisk())
 }
 customElements.define('c-mock', CMock);
 
@@ -15,10 +18,37 @@ describe('CFormQuestion', () => {
             </c-mock>`
     });
 
-    it('should do something', () => {
-        document.querySelector('c-mock input').value += 'test';
-        expect(document.querySelector('c-mock').hasAttribute('data-valid')).toBeTruthy();
+    it('should execute the buildHelperElement method', () => {
+        expect(document.querySelector('c-mock').buildHelperElement).toHaveBeenCalled();
     });
+
+    it('should execute the setHelperText method', () => {
+        expect(document.querySelector('c-mock').setHelperText).toHaveBeenCalled();
+    });
+
+    it('should execute the setAsterisk method', () => {
+        expect(document.querySelector('c-mock').setAsterisk).toHaveBeenCalled();
+    });
+
+    it('should build and append the helper element', () => {
+        const mock = new CMock();
+        mock.buildHelperElement();
+        expect(mock.querySelector('.helper-text')).toBeTruthy();
+    });
+
+    it('should set the helper text status and message', () => {
+        const mock = new CMock();
+        mock.buildHelperElement();
+        mock.setHelperText();
+        expect(mock.querySelector('.font-color-error')).toBeTruthy();
+        expect(mock.querySelector('.helper-text').innerText).toMatch('This field is required');
+    });
+
+    it('should add an asterisk to the label', () => {
+        const mock = new CMock();
+        mock.setAsterisk();
+        expect(mock.querySelector('label').innerText).toContain('*');
+    })
 });
 
 
